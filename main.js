6741dc9e78dc8b2dd8711228000fc41cb738bd62
Sepehr Taban <sepehr.taban77@gmail.com>
2021-07-15 17:55:44 +0430

    add basic functionality for dom manipulation

--- main.js
+++ main.js
@@ -39,6 +39,109 @@ function checkTodayStorage(){
     localStorage.setItem("itemsOfPast", JSON.stringify(itemsOfPast));
     localStorage.setItem("todayItems", JSON.stringify(todayItems));
 }
+/*############################# manipulating DOM ##################################*/
+/*############################# manipulating DOM ##################################*/
+/*############################# manipulating DOM ##################################*/
+/*############################# manipulating DOM ##################################*/
+/*############################# manipulating DOM ##################################*/
+const onMobile = window.innerWidth < 500;
+let currentMenu;
+const additionForm = document.querySelector('.add-item');
+const calculationForm = document.querySelector('.calc-item');
+const deletionForm = document.querySelector('.delet-item');
+const formContainer = document.querySelector('.forms');
+
+const tottalResultAlert = document.querySelector('.total-result');
+const deletionAlert = document.querySelector('.deletMsg');
+const notSavedAnyItemAlert = document.querySelector('.notSavedMsg');
+
+const hambergerMenu = document.querySelector('.hamMenu');
+const navBar = document.querySelector('.navBar');
+
+
+
+const mainTableContainer = document.querySelector('.table-container');
+
+(!todayItems.length) ? tellUserThereIsNoSavedItem() : showTheTableToUser();
+
+hambergerMenu.addEventListener('click', ()=>{
+    toggleNaberMenu()
+})
+
+const menuBtns = Array.from(document.querySelectorAll('.menu-btn'));
+menuBtns.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        currentMenu = btn.textContent;
+        menuBtns.forEach(btn =>{
+            btn.classList.remove('active')
+            btn.disabled = true;
+        });
+        btn.classList.add('active');
+        
+        changeTheForm(); 
+        
+        setTimeout(() => {
+            menuBtns.forEach(btn => btn.disabled = false);              
+        }, 300);
+        
+    })
+})
+
+
+
+function changeTheForm(){
+
+    if(onMobile) toggleNaberMenu();
+    
+    tottalResultAlert.classList.remove('active');
+    deletionAlert.classList.remove('active');
+
+    if(currentMenu === 'Calc Total'){
+        additionForm.classList.remove('active');
+        deletionForm.classList.remove('active');
+        setTimeout(() => {
+            calculationForm.classList.add('active');
+        }, 500);
+    }
+    if(currentMenu === 'Add Item'){
+        calculationForm.classList.remove('active');
+        deletionForm.classList.remove('active');
+        setTimeout(() => {
+            additionForm.classList.add('active');
+        }, 500);
+
+    }
+    if(currentMenu === 'Delete Items'){
+        additionForm.classList.remove('active');
+        calculationForm.classList.remove('active');
+        setTimeout(() => {
+            deletionForm.classList.add('active');
+        }, 500);
+
+    }
+
+    
+    setTimeout(() => {
+        formContainer.classList.add('moveUp');
+    }, 1000);
+}
+
+function tellUserThereIsNoSavedItem(){
+    notSavedAnyItemAlert.classList.add('active');
+    mainTableContainer.classList.remove('active');
+    
+    formContainer.classList.add('moveUpUp');
+}
+function showTheTableToUser(){
+    notSavedAnyItemAlert.classList.remove('active');
+    mainTableContainer.classList.add('active');
+
+    formContainer.classList.add('moveUp');
+}
+function toggleNaberMenu(){
+    hambergerMenu.classList.toggle('active');
+    navBar.classList.toggle('active');
+}
 
 /*######################## getting Dates ##########################*/
 /*######################## getting Dates ##########################*/
